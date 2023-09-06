1786db4aaecb49284f87b87c5d53245416a15ffb
witchpixels <thewitch@witchpixels.dev>
2023-09-06 16:49:50 -0700

    update project version and add version stamps

---
+++ .build/stamp_version.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+if [[ -z "$GitVersion_FullSemVer" ]]; then
+  GitVersion_FullSemVer="0.1.0-failed-to-set-from-gitversion"
+fi
+
+cat ./project.godot | sed "/^config\\/version=/s/=.*/=\"$GitVersion_FullSemVer\"/" > ./project.godot.2
+cp ./project.godot.2 ./project.godot
+rm ./project.godot.2
