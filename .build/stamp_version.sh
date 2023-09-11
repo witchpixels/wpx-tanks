bc1e49ecaf50f598998c47ea6826786f6934cbb7
witchpixels <thewitch@witchpixels.dev>
2023-09-11 01:00:33 -0700

    sh on gitlab runners does not like [[]] if statements.

--- .build/stamp_version.sh
+++ .build/stamp_version.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ -z "$GitVersion_FullSemVer" ]]; then
-  GitVersion_FullSemVer="0.1.0-failed-to-set-from-gitversion"
-fi
-
 cat ./project.godot | sed "/^config\\/version=/s/=.*/=\"$GitVersion_FullSemVer\"/" > ./project.godot.2
 cp ./project.godot.2 ./project.godot
 rm ./project.godot.2
