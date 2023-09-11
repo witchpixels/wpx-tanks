f17010b027f9dade485214571e85458b04890aa6
witchpixels <thewitch@witchpixels.dev>
2023-09-11 00:50:14 -0700

    more prints to maybe help

--- .build/setup_blender_editor_path.sh
+++ .build/setup_blender_editor_path.sh
@@ -2,10 +2,18 @@ BLENDER_PATH=$(which blender) || BLENDER_PATH="\\/usr\\/bin"
 
 godot --headless -e --quit-after 100
 
+echo "EDITOR SETTINGS BEFORE ---------------------------------"
+cat ~/.config/godot/editor_settings-4.tres
+echo "-------------------------------------------------------"
+
 cat ~/.config/godot/editor_settings-4.tres \
     | sed "/^filesystem\\/import\\/blender\\/blender3_path =/s/=.*/= \"$BLENDER_PATH\"/" \
     > ~/.config/godot/editor_settings-4.tres.2
 
 cp ~/.config/godot/editor_settings-4.tres.2 ~/.config/godot/editor_settings-4.tres
+
+
+echo "EDITOR SETTINGS AFTER ---------------------------------"
 cat ~/.config/godot/editor_settings-4.tres
+echo "-------------------------------------------------------"
 rm ~/.config/godot/editor_settings-4.tres.2
